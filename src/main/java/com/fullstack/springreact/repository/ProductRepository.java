017ba0f321262835263bcedebc29d86527a87e8c
ChethanCN <134485903+ChethanCNRepo@users.noreply.github.com>
2023-05-24 18:07:21 +0530

    Update ProductRepository.java

--- src/main/java/com/fullstack/springreact/repository/ProductRepository.java
+++ src/main/java/com/fullstack/springreact/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.fullstack.springreact.entity.Product;
-
+//change
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
